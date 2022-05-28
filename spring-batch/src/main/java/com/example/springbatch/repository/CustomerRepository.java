c6de831d0eb16759f04cbd3a48e944bc56b379d2
lancn95 <catngoclan95@gmail.com>
2022-05-28 14:17:34 +0700

    init spring batch with 1000 records

---
+++ spring-batch/src/main/java/com/example/springbatch/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.springbatch.repository;
+
+import com.example.springbatch.enitty.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
